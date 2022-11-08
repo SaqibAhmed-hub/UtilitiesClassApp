79d7cd04e9ab1ecec4c35120423976a5966e0dd8
Saqib Ahmed <saqibahmed-colan>
2022-11-08 17:35:42 +0530

    code fix

--- app/src/main/java/com/example/utiltiesclassapp/MainActivity.kt
+++ app/src/main/java/com/example/utiltiesclassapp/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.utiltiesclassapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        val TAG: String = MainActivity::class.java.simpleName
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
 }
--- app/src/main/java/com/example/utiltiesclassapp/utils/StringUtils.kt
+++ app/src/main/java/com/example/utiltiesclassapp/utils/StringUtils.kt
@@ -2,28 +2,25 @@ package com.example.utiltiesclassapp.utils
 
 import java.util.regex.Pattern
 
-object StringUtils {
 
-    val EMAIL_ADDRESS_PATTERN = Pattern.compile(
-        "[a-zA-Z0-9\\+\\.\\_\\%\\-\\+]{1,256}" + "\\@"
-                + "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,64}" + "(" + "\\."
-                + "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,25}" + ")+"
-    )
+val EMAIL_ADDRESS_PATTERN = Pattern.compile(
+    "[a-zA-Z0-9\\+\\.\\_\\%\\-\\+]{1,256}" + "\\@"
+            + "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,64}" + "(" + "\\."
+            + "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,25}" + ")+"
+)
 
-    fun String?.checkIsNullOrEmpty(): Boolean {
-        return !this.isNullOrEmpty() && !this.equals("null", ignoreCase = true)
+fun String?.checkIsNullOrEmpty(): Boolean {
+    return !this.isNullOrEmpty() && !this.equals("null", ignoreCase = true)
+}
+
+fun String.removeFirstLastChar(): String = this.substring(1, this.length.minus(1))
+
+fun String.isValidEmail(): Boolean {
+    return EMAIL_ADDRESS_PATTERN.matcher(this).matches()
+}
+
+fun String.captalize(value: String): String {
+    return value.replaceFirstChar {
+        it.uppercase()
     }
-
-    fun String.removeFirstLastChar(): String = this.substring(1, this.length.minus(1))
-
-    fun String.isValidEmail(): Boolean {
-        return EMAIL_ADDRESS_PATTERN.matcher(this).matches()
-    }
-
-    fun String.captalize(value: String): String{
-        return value.replaceFirstChar {
-            it.uppercase()
-        }
-    }
-
 }
